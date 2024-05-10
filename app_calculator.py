2f300626780a39ee9a7e1530e3cd2bec1ba30315
Jannina <jaitabor2033@gmail.com>
2024-05-10 15:41:16 +0800

    Created a Psuedocode

--- app_calculator.py
+++ app_calculator.py
@@ -1,3 +1,14 @@
 # Psuedocode
 
 # Display a welcome message (start of the app) for the user
+# Create a Main Loop for the series of code
+# Prompt the user for their chosen type of arithmetic operation and input two numbers to be calculated
+# Create an operation menu for the user to choose
+# Set Addition as 1
+# Set Subtract as 2
+# Set Multiplication as 3
+# Set Division as 4
+# Prompt the user to choose an operation by entering number from 1 to 4 corresponding the menu
+# If the entered number is invalid, display error message and ask to choose again
+# Prompt the user to enter two numbers 
+# 
